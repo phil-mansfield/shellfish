3ff50ba454ea4cc13db3f360ca95786db6f9d98d
Phil Mansfield <mansfield@uchicago.edu>
2016-04-05 11:52:44 -0500

    Added documentation for cmd outline.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,19 +2,25 @@
 line modes */
 package cmd
 
-type GlobalConfig struct {
-	snapshotFmt, snapshotType string
-	rockstarDir, treeDir, memorDir string
-	snapshotRange []int
-}
-
+// Mode represents the interface used by the main binary when interacting with
+// a given command line mode.
 type Mode interface {
 	// ReadConfig reads a mode-specific config file and stores its contents
 	// within the Mode.
 	ReadConfig(name string) error
+	// ExampleConfig returns the text of an example config file of this mode.
+	ExampleConfig() string
 	// Run executes the mode. It takes a list of tokenized command line flags,
 	// an initialized GlobalConfig struct, and a slice of lines representing the
 	// contents of stdin. It will return a slice of lines that should be
 	// written to stdout along with an error if one occurs.
 	Run(flags []string, gConfig *GlobalConfig, stdin []string) ([]string, error)
+}
+
+// GlobalConfig is a config file used by every mode. It contains information on
+// the directories that various files are stored in.
+type GlobalConfig struct {
+	snapshotFmt, snapshotType string
+	rockstarDir, treeDir, memorDir string
+	snapshotRange []int
 }

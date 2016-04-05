d27a701713169913ad70b103dedb55379f55f6ae
Phil Mansfield <mansfield@uchicago.edu>
2016-04-05 11:27:58 -0500

    Config file parsing package.

---
+++ parse/config.go
@@ -0,0 +1,324 @@
+package parse
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"fmt"
+)
+
+/////////////////////
+// Conversion Code //
+/////////////////////
+
+type varType int
+const (
+	intVar varType = iota
+	intsVar
+	floatVar
+	floatsVar
+	stringVar
+	stringsVar
+	boolVar
+	boolsVar
+)
+
+func (v varType) String() string {
+	switch v {
+	case intVar: return "int"
+	case intsVar: return "int list"
+	case floatVar: return "float"
+	case floatsVar: return "float list"
+	case stringVar: return "string"
+	case stringsVar: return "string list"
+	case boolVar: return "bool"
+	case boolsVar: return "bool list"
+	}
+	panic("Impossible")
+}
+
+type conversionFunc func(string) bool
+
+type ConfigVars struct {
+	name string
+	varNames []string
+	varTypes []varType
+	conversionFuncs []conversionFunc
+}
+
+func intConv(ptr *int64) conversionFunc {
+	return func(s string) bool {
+		i, err := strconv.Atoi(s)
+		if err != nil { return false }
+		*ptr = int64(i)
+		return true
+	}
+}
+
+func floatConv(ptr *float64) conversionFunc {
+	return func(s string) bool {
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil { return false }
+		*ptr = f
+		return true
+	}
+}
+
+func stringConv(ptr *string) conversionFunc {
+	return func(s string) bool {
+		*ptr = strings.Trim(s, " ")
+		return true
+	}
+}
+
+func boolConv(ptr *bool) conversionFunc {
+	return func(s string) bool {
+		b, err := strconv.ParseBool(s)
+		if err != nil { return false }
+		*ptr = b
+		return true
+	}
+}
+
+func strToList(a string) []string {
+	strs := strings.Split(a, ",")
+	for i := range strs {
+		strs[i] = strings.Trim(strs[i], " ")
+	}
+	return strs
+}
+
+func intsConv(ptr *[]int64) conversionFunc {
+	return func(s string) bool {
+		toks := strToList(s)
+		for j := range toks {
+			i, err := strconv.Atoi(toks[j])
+			if err != nil { return false }
+			*ptr = append(*ptr, int64(i))
+		}
+		return true
+	}
+}
+
+func floatsConv(ptr *[]float64) conversionFunc {
+	return func(s string) bool {
+		toks := strToList(s)
+		for j := range toks {
+			f, err := strconv.ParseFloat(toks[j], 64)
+			if err != nil { return false }
+			*ptr = append(*ptr, f)
+		}
+		return true
+	}
+}
+
+func stringsConv(ptr *[]string) conversionFunc {
+	return func(s string) bool {
+		toks := strToList(s)
+		for j := range toks {
+			*ptr = append(*ptr, toks[j])
+		}
+		return true
+	}
+}
+
+
+func boolsConv(ptr *[]bool) conversionFunc {
+	return func(s string) bool {
+		toks := strToList(s)
+		for j := range toks {
+			b, err := strconv.ParseBool(toks[j])
+			if err != nil { return false }
+			*ptr = append(*ptr, b)
+		}
+		return true
+	}
+}
+
+func NewConfigVars(name string) *ConfigVars {
+	return &ConfigVars{name: name}
+}
+
+func (vars *ConfigVars) Int(ptr *int64, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, intConv(ptr))
+	vars.varTypes = append(vars.varTypes, intVar)
+}
+
+func (vars *ConfigVars) Float(ptr *float64, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, floatConv(ptr))
+	vars.varTypes = append(vars.varTypes, floatVar)
+}
+
+func (vars *ConfigVars) String(ptr *string, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, stringConv(ptr))
+	vars.varTypes = append(vars.varTypes, stringVar)
+}
+
+func (vars *ConfigVars) Bool(ptr *bool, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, boolConv(ptr))
+	vars.varTypes = append(vars.varTypes, boolVar)
+}
+
+func (vars *ConfigVars) Ints(ptr *[]int64, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, intsConv(ptr))
+	vars.varTypes = append(vars.varTypes, intsVar)
+}
+
+func (vars *ConfigVars) Floats(ptr *[]float64, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, floatsConv(ptr))
+	vars.varTypes = append(vars.varTypes, floatsVar)
+}
+
+func (vars *ConfigVars) Strings(ptr *[]string, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, stringsConv(ptr))
+	vars.varTypes = append(vars.varTypes, stringsVar)
+}
+
+func (vars *ConfigVars) Bools(ptr *[]bool, name string) {
+	vars.varNames = append(vars.varNames, name)
+	vars.conversionFuncs = append(vars.conversionFuncs, boolsConv(ptr))
+	vars.varTypes = append(vars.varTypes, boolsVar)
+}
+
+//////////////////
+// Parsing Code //
+//////////////////
+
+func ReadConfig(fname string, vars *ConfigVars) error {
+	bs, err := ioutil.ReadFile(fname)
+	if err != nil { return err }
+	lines := strings.Split(string(bs), "\n")
+	lines, lineNums := removeComments(lines)
+	for i := range lineNums { lineNums[i] ++ }
+
+	if len(lines) == 0 || lines[0] != fmt.Sprintf("[%s]", vars.name) {
+		return fmt.Errorf(
+			"I expected the config file %s to have the header " +
+			"[%s] at the top, but didn't find it.", fname, vars.name,
+		)
+	}
+	lines = lines[1:]
+
+	names, vals, errLine := associationList(lines)
+	if errLine !=  -1 {
+		return fmt.Errorf(
+			"I could not parse line %d of the config file %s because it " +
+			"did not take the form of a variable assignment.",
+			lineNums[errLine+1], fname,
+		)
+	}
+
+	if errLine = checkValidNames(names, vars); errLine != -1 {
+		return fmt.Errorf(
+			"Line %d of the config file %s assigns a value to the " +
+			"variable '%s', but config files of type %s don't have that " +
+			"variable.", lineNums[errLine+1], fname, names[errLine], vars.name,
+		)
+	}
+
+	if errLine1, errLine2 := checkDuplicateNames(names); errLine1 != -1 {
+		return fmt.Errorf(
+			"Lines %d and %d of the config file %s both assign a value to " +
+			"the variable '%s'.", lineNums[errLine1+1], lineNums[errLine2+1],
+			fname, names[errLine1],
+		)
+	}
+
+	if errLine = convertAssoc(names, vals, vars); errLine != -1 {
+		j := 0
+		for ; j < len(vars.varNames); j++ {
+			if vars.varNames[j] == names[errLine] { break }
+		}
+		typeName := vars.varTypes[j].String()
+		a := "a"
+		if typeName[0] == 'i' { a = "an" }
+		return fmt.Errorf(
+			"I could not parse line %d of the config file %s because '%s' " +
+			"expects values of type %s and '%s' cannnot be converted to " +
+			"%s %s.", lineNums[errLine+1], fname, vars.varNames[j], typeName,
+			vals[j], a, typeName,
+		)
+	}
+
+	return nil
+}
+
+func removeComments(lines []string) ([]string, []int) {
+	tmp := make([]string, len(lines))
+	copy(tmp, lines)
+	lines = tmp
+
+	for i := range lines {
+		comment := strings.Index(lines[i], "#")
+		if comment == -1 { continue }
+		lines[i] = lines[i][:comment]
+	}
+
+	out, lineNums := []string{}, []int{}
+	for i := range lines {
+		line := strings.Trim(lines[i], " ")
+		if len(line) == 0 { continue }
+		out = append(out, line)
+		lineNums = append(lineNums, i)
+	}
+
+	return out, lineNums
+}
+
+func associationList(lines []string) ([]string, []string, int) {
+	names, vals := []string{}, []string{}
+	for i := range lines {
+		eq := strings.Index(lines[i], "=")
+		if eq == -1 { return nil, nil, i }
+		name := lines[i][:eq]
+		val := ""
+		if len(lines[i]) - 1 > eq { val = lines[i][eq+1:] }
+		names = append(names, strings.ToLower(strings.Trim(name, " ")))
+		if len(names[len(names) - 1]) == 0 { return nil, nil, i }
+		vals = append(vals, strings.Trim(val, " "))
+	}
+	return names, vals, -1
+}
+
+func checkValidNames(names []string, vars *ConfigVars) int {
+	for i := range names {
+		found := false
+		for j := range vars.varNames {
+			if vars.varNames[j] == names[i] {
+				found = true
+				break
+			}
+		}
+		if !found { return i }
+	}
+	return -1
+}
+
+func checkDuplicateNames(names []string) (int, int) {
+	for i := range names {
+		for j := i + 1; j < len(names); j++ {
+			if names[i] == names[j] { return i, j }
+		}
+	}
+	return -1, -1
+}
+
+func convertAssoc(names, vals []string, vars *ConfigVars) int {
+	for i := range names {
+		j := 0
+		for ; j < len(vars.varNames); j++ {
+			if vars.varNames[j] == names[i] { break }
+		}
+
+		ok := vars.conversionFuncs[j](vals[i])
+		if !ok { return i }
+	}
+	return -1
+}
---
+++ parse/config_test.go
@@ -0,0 +1,346 @@
+package parse
+
+import (
+	"fmt"
+	"testing"
+	"math"
+)
+
+func TestIntConv(t *testing.T) {
+	var x int64
+	ok := intConv(&x)("41891")
+	if !ok { t.Errorf("intConv unsuccessful on valid input.") }
+	if x != 41891 { t.Errorf("intConv did not write input to pointer.") }
+	ok = intConv(&x)("meow")
+	if ok { t.Errorf("intConv successful on invalid input.") }
+}
+
+func TestFloatConv(t *testing.T) {
+	var x float64
+	ok := floatConv(&x)("41891.0")
+	if !ok { t.Errorf("floatConv unsuccessful on valid input.") }
+	if x != 41891.0 { t.Errorf("floatConv did not write input to pointer.") }
+	ok = floatConv(&x)("meow")
+	if ok { t.Errorf("floatConv successful on invalid input.") }
+}
+
+func TestStringConv(t *testing.T) {
+	var x string
+	ok := stringConv(&x)("  41891")
+	if !ok { t.Errorf("stringConv unsuccessful on valid input.") }
+	if x != "41891" { t.Errorf("stringConv did not write input to pointer.") }
+}
+
+func TestBoolConv(t *testing.T) {
+	var x bool
+	ok := boolConv(&x)("true")
+	if !ok { t.Errorf("boolConv unsuccessful on valid input.") }
+	if x != true { t.Errorf("boolConv did not write input to pointer.") }
+	ok = boolConv(&x)("meow")
+	if ok { t.Errorf("boolConv successful on invalid input.") }
+}
+
+func TestIntsConv(t *testing.T) {
+	var x []int64
+	ok := intsConv(&x)("1, 2 , 3")
+	if !ok { t.Errorf("intsConv unsuccesful on valid input.") }
+	if len(x) != 3 || x[0] != 1 || x[1] != 2 || x[2] != 3 {
+		t.Errorf("intsConv did not write input to pointer.")
+	}
+	ok = intsConv(&x)("1,meow,3")
+	if ok { t.Errorf("intsConv successful on invalid input.") }
+}
+
+func TestFloatsConv(t *testing.T) {
+	var x []float64
+	ok := floatsConv(&x)("1, 2.5 , 3")
+	if !ok { t.Errorf("floatsConv unsuccesful on valid input.") }
+	if len(x) != 3 || x[0] != 1 || x[1] != 2.5 || x[2] != 3 {
+		t.Errorf("floatsConv did not write input to pointer.")
+	}
+	ok = floatsConv(&x)("1,meow,3")
+	if ok { t.Errorf("floatsConv successful on invalid input.") }
+}
+
+func TestStringsConv(t *testing.T) {
+	var x []string
+	ok := stringsConv(&x)("dorothy, maddy , sahil")
+	if !ok { t.Errorf("intsConv unsuccesful on valid input.") }
+	if len(x) != 3 || x[0] != "dorothy" || x[1] != "maddy" || x[2] != "sahil" {
+		t.Errorf("intsConv did not write input to pointer.")
+	}
+}
+
+func TestBoolsConv(t *testing.T) {
+	var x []bool
+	ok := boolsConv(&x)("true, false,    true")
+	if !ok { t.Errorf("intsConv unsuccesful on valid input.") }
+	if len(x) != 3 || x[0] != true || x[1] != false || x[2] != true {
+		t.Errorf("intsConv did not write input to pointer.")
+	}
+	ok = boolsConv(&x)("true,meow,false")
+	if ok { t.Errorf("intsConv successful on invalid input.") }
+}
+
+func stringsEq(xs, ys []string) bool {
+	if len(xs) != len(ys) { return false }
+	for i := range xs {
+		if xs[i] != ys[i] { return false }
+	}
+	return true
+}
+
+func intsEq(xs, ys []int) bool {
+	if len(xs) != len(ys) { return false }
+	for i := range xs {
+		if xs[i] != ys[i] { return false }
+	}
+	return true
+}
+
+func TestRemoveComments(t *testing.T) {
+	table := []struct {
+		in, out []string
+		lineNums []int
+	} {
+		{[]string{}, []string{}, []int{}},
+		{[]string{"meow"}, []string{"meow"}, []int{0}},
+		{[]string{"#meow"}, []string{}, []int{}},
+		{[]string{"meow", " # comment", "", "   mew "},
+			[]string{"meow", "mew"}, []int{0, 3}},
+	}
+
+	for i := range table {
+		res, lineNums := removeComments(table[i].in)
+		if !stringsEq(table[i].out, res) {
+			t.Errorf("%d) Called removeComments(%v), got %v",
+				i + 1, table[i].in, res)
+		}
+		if !intsEq(table[i].lineNums, lineNums) {
+			t.Errorf("%d) Called removeComments(%v), got %v linenNums",
+				i +1, table[i].in, lineNums)
+		}
+	}
+}
+
+func TestAssociationList(t *testing.T) {
+	table := []struct {
+		lines []string
+		names, vals []string
+		errLine int
+	} {
+		{[]string{"a=b"}, []string{"a"}, []string{"b"}, -1},
+		{[]string{"a"}, []string{}, []string{}, 0},
+		{[]string{"=b"}, []string{}, []string{}, 0},
+		{[]string{"a=b", "c=", " a = "},
+			[]string{"a", "c", "a"},
+			[]string{"b", "", ""}, -1},
+	}
+
+	for i := range table {
+		names, vals, errLine := associationList(table[i].lines)
+		if errLine != table[i].errLine {
+			t.Errorf("%d) Expected errLine = %d, got %d",
+				i+1, table[i].errLine, errLine)
+		}
+		if errLine != -1 { continue }
+
+		if !stringsEq(names, table[i].names) {
+			t.Errorf("%d) Expected names = %v, got %v.",
+				i+1, table[i].names, names)
+		}
+		if !stringsEq(vals, table[i].vals) {
+			t.Errorf("%d) Expected vals = %v, got %v.",
+				i+1, table[i].vals, vals)
+		}
+
+	}
+}
+
+func TestCheckDuplicateNames(t *testing.T) {
+	table := []struct {
+		names []string
+		i, j int
+	} {
+		{[]string{"a", "b", "c"}, -1, -1},
+		{[]string{"a", "b", "b", "c", "c"}, 1, 2},
+	}
+
+	for k := range table {
+		i, j := checkDuplicateNames(table[k].names)
+		if i != table[k].i || j != table[k].j {
+			t.Errorf("%d) expected (i, j) = (%d, %d) but got (%d, %d)",
+				k+1, table[k].i, table[k].j, i, j)
+		}
+	}
+}
+
+func TestCheckValidNames(t *testing.T) {
+	table := []struct {
+		names, vars []string
+		i int
+	} {
+		{[]string{"a", "b", "c"}, []string{"a", "b", "c", "d"}, -1},
+		{[]string{"a", "b", "c"}, []string{"a", "b", "d"}, 2},
+		{[]string{"a", "a", "a"}, []string{"a", "b", "c", "d"}, -1},
+	}
+
+	for j := range table {
+		vars := &ConfigVars{ varNames: table[j].vars }
+		i := checkValidNames(table[j].names, vars)
+		if i != table[j].i {
+			t.Errorf("%d) expected i = %d, but got %d", j+1, i, table[j].i)
+		}
+	}
+}
+
+func TestConvertAssoc(t *testing.T) {
+	table := []struct {
+		names, vals []string
+		i int
+		xVal int64
+	} {
+		{[]string{"a"}, []string{"3"}, -1, 3},
+		{[]string{"a", "a"}, []string{"3", "meow"}, 1, 3},
+	}
+
+	config := struct { x int64 }{}
+	vars := NewConfigVars("meow")
+	vars.Int(&config.x, "a")
+
+	for j := range table {
+		config.x = 0
+		i := convertAssoc(table[j].names, table[j].vals, vars)
+		if i != table[j].i {
+			t.Errorf("%d) expected errLine = %d, but got %d",
+				j+1, table[j].i, i)
+		}
+		if i != -1 { continue }
+		if config.x != table[j].xVal {
+			t.Errorf("%d) expected config.x = %d, got %d",
+				j+1, config.x, table[j].xVal)
+		}
+	}
+}
+
+func floatEq(x, y, eps float64) bool {
+	return math.Abs(x - y) < eps
+}
+
+func floatsEq(xs, ys []float64, eps float64) bool {
+	if len(xs) != len(ys) { return false }
+	for i := range xs {
+		if !floatEq(xs[i], ys[i], eps) { return false }
+	}
+	return true
+}
+
+func boolsEq(xs, ys []bool) bool {
+	if len(xs) != len(ys) { return false }
+	for i := range xs {
+		if xs[i] != ys[i] { return false }
+	}
+
+	return true
+}
+
+
+func int64sEq(xs, ys []int64) bool {
+	if len(xs) != len(ys) { return false }
+	for i := range xs {
+		if xs[i] != ys[i] { return false }
+	}
+	return true
+}
+
+type testConfig struct {
+	float float64
+	floats []float64
+	num int64
+	nums []int64
+	okay bool
+	okays []bool
+	word string
+	words []string
+}
+
+func makeTestConfig() (*testConfig, *ConfigVars) {
+	config := &testConfig{}
+	vars := NewConfigVars("config")
+	vars.Int(&config.num, "num")
+	vars.Ints(&config.nums, "nums")
+	vars.Float(&config.float, "float")
+	vars.Floats(&config.floats, "floats")
+	vars.Bool(&config.okay, "okay")
+	vars.Bools(&config.okays, "okays")
+	vars.String(&config.word, "word")
+	vars.Strings(&config.words, "words")
+
+	return config, vars
+}
+
+func TestValidConfig(t *testing.T) {
+	config, vars := makeTestConfig()
+	err := ReadConfig("config_test_files/success.config", vars)
+	if err != nil {
+		t.Errorf("Expected successful read of config file, but got " +
+			"error:\n %s", err.Error())
+	}
+
+	if !floatEq(config.float, -1.2e4, 1) {
+		t.Errorf("Expected float = %g, but got %g", -1.2e4, config.float)
+	}
+	if !floatsEq([]float64{2.5, 2.5, 2.5}, config.floats, 0.001) {
+		t.Errorf("Expected floats = %v, but got %v.",
+			[]float64{2.5, 2.5, 2.5}, config.floats)
+	}
+
+	if config.num != 3 {
+		t.Errorf("Expected num = %d, but got %d", 3, config.num)
+	}
+	if !int64sEq(config.nums, []int64{1, 1, 2, 3, 5}) {
+		t.Errorf("Expected nums = %v, but got %v",
+			[]int64{1, 1, 2, 3, 5}, config.nums)
+	}
+
+	if config.okay != true {
+		t.Errorf("Expected okay = %v, but got %v", true, config.okay)
+	}
+	if !boolsEq(config.okays, []bool{true, false, true}) {
+		t.Errorf("Expected okays = %v, buf got %v",
+			[]bool{true, false, true}, config.okays)
+	}
+
+	if config.word != "meow" {
+		t.Errorf("Expected word = %v, but got %v", "meow", config.word)
+	}
+	if !stringsEq([]string{"dorothy", "maddy", "sahil"}, config.words) {
+		t.Errorf("Expected words = %v, but got %v",
+			[]string{"dorothy", "maddy", "sahil"}, config.words)
+	}
+}
+
+func TestInvalidConfig(t *testing.T) {
+	_, vars := makeTestConfig()
+
+	fnames := []string {
+		"config_test_files/empty.config",
+		"config_test_files/wrong_header.config",
+		"config_test_files/non_assignment.config",
+		"config_test_files/no_variable.config",
+		"config_test_files/dupicates.config",
+		"config_test_files/invalid_var.config",
+		"config_test_files/invalid_type.config",
+	}
+
+	for i := range fnames {
+		err := ReadConfig(fnames[i], vars)
+		if err == nil {
+			t.Errorf("No error was reported when attempting to parse %s",
+				fnames[i])
+		} else if testing.Verbose() {
+			fmt.Printf("%s:\n", fnames[i])
+			fmt.Println(err.Error())
+		}
+	}
+}

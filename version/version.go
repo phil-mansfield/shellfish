6b06e00595268dee7ca3b59aabae550c21bdc563
Phil Mansfield <mansfield@uchicago.edu>
2016-04-05 16:13:37 -0500

    Wrote and tested small semantic versioning package.

---
+++ version/version.go
@@ -0,0 +1,54 @@
+/*package version controls the version*/
+package version
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// SourceVersion is the version string representing the semantic version number
+// of the source code.
+const SourceVersion = "0.2.0"
+
+// Parse parses a semantic version number string and returns an error if
+// the string is invalid.
+func Parse(s string) (major, minor, patch int, err error) {
+	toks := strings.Split(s, ".")
+	errMsg := "Version string does not take the form of three " +
+		"period-separated non-negative numbers"
+
+	if len(toks) != 3 { return -1, -1, -1, fmt.Errorf(errMsg) }
+
+	major, err = strconv.Atoi(toks[0])
+	if err != nil { return -1, -1, -1, fmt.Errorf(errMsg) }
+	minor, err = strconv.Atoi(toks[1])
+	if err != nil { return -1, -1, -1, fmt.Errorf(errMsg) }
+	patch, err = strconv.Atoi(toks[2])
+	if err != nil { return -1, -1, -1, fmt.Errorf(errMsg) }
+
+	if major < 0 || minor < 0 || patch < 0 {
+		return -1, -1, -1, fmt.Errorf(errMsg)
+	}
+
+	return major, minor, patch, nil
+}
+
+// Greater returns true if s1 represents a later version of the source than
+// s2. An error is returned if either is invalid.
+func Later(s1, s2 string) (bool, error) {
+	major1, minor1, patch1, err := Parse(s1)
+	if err != nil { return false, err }
+	major2, minor2, patch2, err := Parse(s2)
+	if err != nil { return false, err }
+
+	if major1 == major2 {
+		if minor1 == minor2 {
+			return patch1 > patch2, nil
+		} else {
+			return minor1 > minor2, nil
+		}
+	} else {
+		return major1 > major2, nil
+	}
+}
---
+++ version/version_test.go
@@ -0,0 +1,73 @@
+package version
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		s string
+		major, minor, patch int
+		valid bool
+	} {
+		{"0.0.0", 0, 0, 0, true},
+		{"1.02.3", 1, 2, 3, true},
+		{"", 0, 0, 0, false},
+		{"0", 0, 0, 0, false},
+		{"0.0", 0, 0, 0, false},
+		{"0.0.0.0", 0, 0, 0, false},
+		{"0.-1.0", 0, 0, 0, false},
+	}
+
+	for i := range tests {
+		major, minor, patch, err := Parse(tests[i].s)
+		if err != nil {
+			if tests[i].valid {
+				t.Errorf("Expected Parse('%s') to give an error, but it " +
+					"doesn't.", tests[i].s)
+			}
+		} else {
+			if !tests[i].valid {
+				t.Errorf("Expected Parse('%s') to be valid, but it gave an " +
+					"error.", tests[i].s)
+			}
+			if major != tests[i].major || minor != tests[i].minor ||
+				patch != tests[i].patch {
+				t.Errorf("Parse('%s') parsed to (%d, %d, %d).",
+					tests[i].s, major, minor, patch)
+			}
+		}
+	}
+}
+
+func TestLater(t *testing.T) {
+	tests := []struct {
+		s1, s2 string
+		later, valid bool
+	} {
+		{"0.0.0", "0.0", false, false},
+		{"0.0.0", "0.0.0", false, true},
+		{"0.0.1", "0.0.0", true, true},
+		{"0.1.0", "0.0.0", true, true},
+		{"1.0.0", "0.0.0", true, true},
+		{"0.0.0", "0.0.1", false, true},
+		{"0.0.0", "0.1.0", false, true},
+		{"0.0.0", "1.0.0", false, true},
+		{"2.13.7", "2.12.19", true, true},
+		{"2.12.19", "2.13.7", false, true},
+	}
+
+	for i := range tests {
+		later, err := Later(tests[i].s1, tests[i].s2)
+		if err == nil && !tests[i].valid {
+			t.Errorf("Expected Later('%s', %s) to return an error, but it " +
+				"didn't.", tests[i].s1, tests[i].s2)
+		} else if err != nil && tests[i].valid {
+			t.Errorf("Did not expect Later('%s', '%s') to return an error, " +
+				"but it did.", tests[i].s1, tests[i].s2)
+		} else if later != tests[i].later {
+			t.Errorf("Later('%s', '%s') returned %v", tests[i].s1,
+				tests[i].s2, tests[i].later)
+		}
+	}
+}

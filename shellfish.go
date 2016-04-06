3394e73ffebdd8b8a55734b95e2a0b209795301f
Phil Mansfield <mansfield@uchicago.edu>
2016-04-05 22:39:00 -0500

    Added all I/O code to the command line tool.

--- shellfish.go
+++ shellfish.go
@@ -3,8 +3,12 @@ halos in N-body simulations.*/
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"fmt"
+	"strings"
+
+	"github.com/phil-mansfield/shellfish/cmd"
 )
 
 func modeDescriptions() string {
@@ -14,18 +18,12 @@ func modeDescriptions() string {
 	"My setup mode is:\n" +
 	"$ ./shellfish setup ____.setup.config\n\n" +
 
-	"My shell-finding modes are:\n" +
-	"$ ./shellfish id    [flags] ____.config [____.id.config]\n" +
-	"$ ./shellfish tree  [flags] ____.config [____.tree.config]\n" +
-	"$ ./shellfish shell [flags] ____.config [____.shell.config]\n" +
-	"$ ./shellfish stats [flags] ____.config [____.stats.config]\n\n" +
-
-	"I have a few other modes which are convenient for this type of " +
-	"analysis:\n" +
-	"$ ./shellfish append [flags] ____.config [____.append.config]\n" +
-	"$ ./shellfish prof   [flags] ____.config [____.id.config]\n" +
-	"$ ./shellfish render [flags] ____.config [____.render.config]\n" +
-	"$ ./shellfish phase  [flags] ____.config [____.phase]\n"
+	"My other modes are:\n" +
+	"$ ./shellfish id     [flags] ____.config [____.id.config]\n" +
+	"$ ./shellfish tree   [flags] ____.config [____.tree.config]\n" +
+	"$ ./shellfish shell  [flags] ____.config [____.shell.config]\n" +
+	"$ ./shellfish stats  [flags] ____.config [____.stats.config]\n\n" +
+	"$ ./shellfish append [flags] ____.config [____.append.config]\n"
 }
 
 func main() {
@@ -38,13 +36,114 @@ func main() {
 		os.Exit(1)
 	}
 
-	switch args[1] {
-	case "":
-	default:
+
+	if args[1] == "setup" {
+		// TODO: Implement the setup command.
+		panic("NYI")
+	} else if args[1] == "help" {
+		// TODO: Implement the help command.
+	}
+
+	mode, ok := cmd.ModeNames[args[1]]
+	if !ok {
 		fmt.Fprintf(
 			os.Stderr, "You passed me the mode '%s', which I don't " +
 			"recognize.\nFor help, type './shellfish help'\n", args[1],
 		)
 		os.Exit(1)
 	}
+
+	lines, err := stdinLines()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	flags := getFlags(args)
+	gConfig, err := getGlobalConfig(args)
+	if err != nil { fmt.Fprintf(os.Stderr, err.Error()) }
+	config, ok := getConfig(args)
+
+	if ok {
+		if err = mode.ReadConfig(config); err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	}
+
+	out, err := mode.Run(flags, gConfig, lines)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	for i := range out { fmt.Println(out[i]) }
+}
+
+
+// stdinLines reads stdin and splits it into lines.
+func stdinLines() ([]string, error) {
+	bs, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"Error reading stdin: %s.", err.Error(),
+		)
+	}
+	text := string(bs)
+	lines := strings.Split(text, "\n")
+	if lines[len(lines) - 1] == "" { lines = lines[:len(lines) - 1] }
+	return lines, nil
+}
+
+// getFlags reutrns the flag tokens from the command line arguments.
+func getFlags(args []string) ([]string) {
+	return args[1:len(args) - 1 - configNum(args)]
+}
+
+// getGlobalConfig returns the name of the base config file from the command
+// line arguments.
+func getGlobalConfig(args []string) (*cmd.GlobalConfig, error) {
+	name := ""
+	switch configNum(args) {
+	case 0:
+		return nil, fmt.Errorf("No config files provided in command " +
+			"line arguments")
+	case 1:
+		name = args[len(args) - 1]
+	case 2:
+		name = args[len(args) - 2]
+	}
+
+	config := &cmd.GlobalConfig{}
+	err := config.ReadConfig(name)
+	if err != nil { return nil, err }
+	return config, nil
+}
+
+// getConfig return the name of the mode-specific config file from the command
+// line arguments.
+func getConfig(args []string) (string, bool) {
+	if configNum(args) == 2 {
+		return args[len(args) - 1], true
+	}
+	return "", false
+}
+
+// configNum returns the number of configuration files at the end of the
+// argument list (up to 2).
+func configNum(args []string) int {
+	num := 0
+	for i := len(args) - 1; i >= 0 && i >= len(args) - 2; i-- {
+		if isConfig(args[i]) {
+			num++
+		} else {
+			break
+		}
+	}
+	return num
+}
+
+// isConfig returns true if the fiven string is a config file name.
+func isConfig(s string) bool {
+	return len(s) >= 7 &&  s[len(s) - 7:] == ".config"
 }

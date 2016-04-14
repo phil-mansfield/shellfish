8d88c4b684d9f98bb7d900255377f607aaf91e48
Phil Mansfield <mansfield@uchicago.edu>
2016-04-14 17:32:18 -0500

    Wrote shell mode.

--- cmd/shell.go
+++ cmd/shell.go
@@ -2,8 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"math"
+	"runtime"
+	"sort"
+
 
 	"github.com/phil-mansfield/shellfish/parse"
+	"github.com/phil-mansfield/shellfish/cmd/env"
+	"github.com/phil-mansfield/shellfish/cmd/catalog"
+	"github.com/phil-mansfield/shellfish/cmd/memo"
+
+	"github.com/phil-mansfield/shellfish/los"
+	sph "github.com/phil-mansfield/shellfish/los/sphere_halo"
+	"github.com/phil-mansfield/shellfish/los/analyze"
+
+	"github.com/phil-mansfield/shellfish/render/io"
+	"github.com/phil-mansfield/shellfish/render/halo"
+
+	"github.com/phil-mansfield/shellfish/math/rand"
+
 )
 
 type ShellConfig struct {
@@ -13,7 +30,7 @@ type ShellConfig struct {
 
 	eta float64
 	order, smoothingWindow, levels, subsampleFactor int64
-	losSlopeCutoff float64
+	losSlopeCutoff, backgroundRhoMult float64
 }
 
 var _ Mode = &ShellConfig{}
@@ -71,8 +88,13 @@ Levels = 3
 SmoothingWindow = 121
 
 # Cutoff is the minimum slope allowed when finding the point of steepest slope
-# for individual lines of sight.
-LOSSlopeCutoff = 0.0`
+# for individual lines of sight. Of all the parameters that should not be
+# changed, this is the one which should not be changed the most.
+LOSSlopeCutoff = 0.0
+
+# BackgroundRhoMult is the density assigned to points which do not intersect
+# with any kernels as a multiple of the kernel density.
+BackgroundRhoMult = 0.5`
 }
 
 func (config *ShellConfig) ReadConfig(fname string) error {
@@ -90,6 +112,7 @@ func (config *ShellConfig) ReadConfig(fname string) error {
 	vars.Int(&config.levels, "Levels", 3)
 	vars.Int(&config.smoothingWindow, "SmoothingWindow", 121)
 	vars.Float(&config.losSlopeCutoff, "LOSSlopeCutoff", 0.0)
+	vars.Float(&config.backgroundRhoMult, "BackgroundRhoMult", 0.5)
 
 	if err := parse.ReadConfig(fname, vars); err != nil { return err }
 	return config.validate()
@@ -144,5 +167,369 @@ func (config *ShellConfig) validate() error {
 func (config *ShellConfig) Run(
 	flags []string, gConfig *GlobalConfig, stdin []string,
 ) ([]string, error) {
-	panic("NYI")
+
+	e := &env.Environment{MemoDir: gConfig.memoDir}
+	e.InitGotetra(
+		gConfig.snapshotFormat, gConfig.snapMin, gConfig.snapMax,
+		gConfig.formatMins, gConfig.formatMaxes, gConfig.validateFormats,
+	)
+	e.InitRockstar(
+		gConfig.haloDir, gConfig.snapMin, gConfig.snapMax,
+	)
+
+		// Parse.
+	intCols, _, err := catalog.ParseCols(stdin, []int{0, 1}, []int{})
+	if err != nil { return nil, err }
+	ids, snaps := intCols[0], intCols[1]
+
+	if len(ids) == 0 { return nil, fmt.Errorf("No input IDs.") }
+
+	// Compute coefficients.
+	out := make([][]float64, len(ids))
+
+
+	rowLength := config.order*config.order*2
+
+	for i := range out {
+		out[i] = make([]float64, rowLength)
+	}
+
+	err = loop(ids, snaps, config, e, out)
+	if err != nil { return nil, err }
+
+	floatNames, intNames := []string{"ID", "Snapshot"}, []string{}
+	for k := 0; k < 2; k++ {
+		for j := 0; j < int(config.order); j++ {
+			for i := 0; i < int(config.order); i++ {
+				floatNames = append(
+					floatNames, fmt.Sprintf("P_i=%d,j=%d,k=%d", i, j, k),
+				)
+			}
+		}
+	}
+
+	lines := catalog.FormatCols(
+		[][]int{ids, snaps}, [][]float64{}, []int{0, 1},
+	)
+
+	colOrder := make([]int, 2 + 2*config.order*config.order)
+	for i := range colOrder { colOrder[i] = i }
+	cString := catalog.CommentString(intNames, floatNames, colOrder)
+	return append([]string{cString}, lines...), nil
+}
+
+
+func loop(
+	ids, snaps []int, c *ShellConfig, e *env.Environment, out [][]float64,
+) error {
+	snapBins, idxBins := binBySnap(snaps, ids)
+	ringBuf := make([]analyze.RingBuffer, c.rings)
+	for i := range ringBuf { ringBuf[i].Init(int(c.spokes), int(c.radialBins)) }
+
+	sortedSnaps := []int{}
+	for snap := range snapBins {
+		sortedSnaps = append(sortedSnaps, snap)
+	}
+	sort.Ints(sortedSnaps)
+
+	hds, _, err := memo.ReadHeaders(sortedSnaps[0], e)
+	if err != nil { return err }
+
+	workers := runtime.NumCPU()
+	gw := hds[0].GridWidth
+	sphBuf := &sphBuffers{
+		intr: make([]bool, gw * gw * gw),
+		vecs: make([][3]float32, gw * gw * gw),
+		sphWorkers: make([]sph.SphereHalo, workers - 1),
+	}
+
+	for _, snap := range sortedSnaps {
+		if snap == -1 { continue }
+		snapIDs := snapBins[snap]
+		idxs := idxBins[snap]
+
+		// Create Halos
+		runtime.GC()
+		halos, err := createHalos(snap, &hds[0], snapIDs, c, e)
+		if err != nil { return err }
+
+		if err = sphereLoop(snap, ids, idxs, halos, c, e, sphBuf, out);
+			err != nil { return err }
+
+		// Analysis
+		if err = haloAnalysis(halos, idxs, c, ringBuf, out); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func sphereLoop(
+	snap int, IDs, ids []int, halos []los.Halo, c *ShellConfig,
+	e *env.Environment, sphBuf *sphBuffers, out [][]float64,
+) error {
+	hds, files, err := memo.ReadHeaders(snap, e)
+	if err != nil { return err }
+	intrBins := binIntersections(hds, halos)
+
+	for i := range hds {
+		runtime.GC()
+		if len(intrBins[i]) == 0 { continue }
+
+		err := io.ReadSheetPositionsAt(files[i], sphBuf.vecs)
+		if err != nil { return err }
+
+		binHs := intrBins[i]
+		for j := range binHs {
+			h, ok := binHs[j].(*sph.SphereHalo)
+			if !ok { panic("Invalid Halo interface given to sphereLoop().") }
+			loadSphereVecs(h, sphBuf, &hds[i], c)
+		}
+
+	}
+
+	return nil
+}
+
+type sphBuffers struct {
+	sphWorkers []sph.SphereHalo
+	vecs [][3]float32
+	intr []bool
+}
+
+func loadSphereVecs(
+	h *sph.SphereHalo, sphBuf *sphBuffers, hd *io.SheetHeader, c *ShellConfig,
+) {
+	workers := runtime.NumCPU()
+	runtime.GOMAXPROCS(workers)
+	sphWorkers, vecs, intr := sphBuf.sphWorkers, sphBuf.vecs, sphBuf.intr
+	if len(sphWorkers) + 1 != workers { panic("impossible")}
+
+	sync := make(chan bool, workers)
+
+	h.Transform(vecs, hd.TotalWidth)
+	rad := h.RMax() * c.rKernelMult / c.rMaxMult
+	h.Intersect(vecs, rad, intr)
+
+	counts := zCounts(sphBuf.intr, int(hd.GridWidth))
+	zIdxs := zSplit(counts, workers)
+
+	h.Split(sphWorkers)
+
+	for i := range sphWorkers {
+		wh := &sphBuf.sphWorkers[i]
+		go chanLoadSphereVec(wh, vecs, intr, zIdxs[i], hd, c, sync)
+	}
+	chanLoadSphereVec(h, vecs, intr, zIdxs[workers-1], hd, c, sync)
+
+	for i := 0; i < workers; i++ { <-sync }
+
+	h.Join(sphWorkers)
+}
+
+func chanLoadSphereVec(
+	h *sph.SphereHalo, vecs [][3]float32, intr []bool,
+	zIdxs []int, hd *io.SheetHeader, c *ShellConfig, sync chan bool,
+) {
+	gw, sw := int(hd.GridWidth), int(hd.SegmentWidth)
+
+	rad := h.RMax() * c.rKernelMult / c.rMaxMult
+	sphVol := 4*math.Pi/3*rad*rad*rad
+
+	pl := hd.TotalWidth / float64(int(hd.CountWidth) / int(c.subsampleFactor))
+	pVol := pl*pl*pl
+
+	rho := pVol / sphVol
+	for _, z := range zIdxs {
+		if z >= sw { continue }
+
+		for y := 0; y < sw; y++ {
+			for x := 0; x < sw; x++ {
+
+				idx := x + y*gw + z*gw*gw
+				if intr[idx] { h.Insert(vecs[idx], rad, rho) }
+			}
+		}
+	}
+
+	sync <- true
+}
+
+func haloAnalysis(
+	halos []los.Halo, idxs []int, c *ShellConfig,
+	ringBuf []analyze.RingBuffer, out [][]float64,
+) error {
+	// Calculate Penna coefficients.
+	for i := range halos {
+		runtime.GC()
+		var ok bool
+		out[idxs[i]], ok = calcCoeffs(halos[i], ringBuf, c)
+		if !ok {
+			fmt.Errorf("Shell coefficients undetermined. The most likely " +
+				"explanation is that there is corruption in your particle " +
+				"snapshots.")
+		}
+	}
+	return nil
+}
+
+func createHalos(
+	snap int, hd *io.SheetHeader, ids []int, c *ShellConfig, e *env.Environment,
+) ([]los.Halo, error) {
+	vals, err := memo.ReadRockstar(
+		snap, ids, e, halo.X, halo.Y, halo.Z, halo.Rad200b,
+	)
+	if err != nil { return nil, err }
+
+	xs, ys, zs, rs := vals[0], vals[1], vals[2], vals[3]
+
+	// Initialize halos.
+	halos := make([]los.Halo, len(ids))
+
+	for i, _ := range ids {
+		if rs[i] <= 0 { continue }
+
+		norms := normVecs(int(c.rings))
+		origin := [3]float64{
+			float64(xs[i]), float64(ys[i]), float64(zs[i]),
+		}
+		rMax, rMin := rs[i] * c.rMaxMult, rs[i] * c.rMinMult
+		rad := rs[i]*c.rKernelMult
+		sphVol := 4*math.Pi/3*rad*rad*rad
+		pl := hd.TotalWidth/float64(int(hd.CountWidth)/int(c.subsampleFactor))
+		pVol := pl*pl*pl
+		rho := pVol / sphVol
+		defaultRho := rho * c.backgroundRhoMult
+
+		halo := &sph.SphereHalo{}
+		halo.Init(norms, origin, rMin, rMax, int(c.radialBins),
+			int(c.spokes), defaultRho)
+
+		halos[i] = halo
+	}
+
+	return halos, nil
+}
+
+func normVecs(n int) [][3]float32 {
+	var vecs [][3]float32
+	gen := rand.NewTimeSeed(rand.Xorshift)
+	switch n {
+	case 3:
+		vecs = [][3]float32{{0, 0, 1}, {0, 1, 0}, {1, 0, 0}}
+	default:
+		vecs = make([][3]float32, n)
+		for i := range vecs {
+			for {
+				x := gen.Uniform(-1, +1)
+				y := gen.Uniform(-1, +1)
+				z := gen.Uniform(-1, +1)
+				r := math.Sqrt(x*x + y*y + z*z)
+
+				if r < 1 {
+					vecs[i] = [3]float32{
+						float32(x/r), float32(y/r), float32(z/r),
+					}
+					break
+				}
+			}
+		}
+	}
+
+	return vecs
+}
+
+type profileRange struct {
+	rMin, rMax float64
+	v0 [3]float32
+}
+
+// Used for load balancing.
+func zCounts(grid []bool, n int) []int {
+	counts := make([]int, n)
+
+	i := 0
+	for z := 0; z < n; z++ {
+		for y := 0; y < n; y++ {
+			for x := 0; x < n; x++ {
+				if grid[i] { counts[z]++ }
+				i++
+			}
+		}
+	}
+
+	return counts
+}
+
+// Used for load balanacing.
+func zSplit(zCounts []int, workers int) [][]int {
+	tot := 0
+	for _, n := range zCounts { tot += n }
+
+	splits := make([]int, workers + 1)
+	si := 1
+	splitWidth := tot / workers
+	if splitWidth * workers < tot { splitWidth++ }
+	target := splitWidth
+
+	sum := 0
+	for i, n := range zCounts {
+		sum += n
+		if sum > target {
+			splits[si] = i
+			for sum > target { target += splitWidth }
+			si++
+		}
+	}
+	for ; si < len(splits); si++ { splits[si] = len(zCounts) }
+
+	splitIdxs := make([][]int, workers)
+	for i := range splitIdxs {
+		jStart, jEnd := splits[i], splits[i + 1]
+		for j := jStart; j < jEnd; j++ {
+			if zCounts[j] > 0 { splitIdxs[i] = append(splitIdxs[i], j) }
+		}
+	}
+
+	return splitIdxs
+}
+
+func binIntersections(
+hds []io.SheetHeader, halos []los.Halo,
+) [][]los.Halo {
+
+	bins := make([][]los.Halo, len(hds))
+	for i := range hds {
+		for hi := range halos {
+			if halos[hi].SheetIntersect(&hds[i]) {
+				bins[i] = append(bins[i], halos[hi])
+			}
+		}
+	}
+	return bins
+}
+
+func calcCoeffs(
+	halo los.Halo, buf []analyze.RingBuffer, c *ShellConfig,
+) ([]float64, bool) {
+	for i := range buf {
+		buf[i].Clear()
+		buf[i].Splashback(halo, i, int(c.smoothingWindow), c.losSlopeCutoff)
+	}
+	pxs, pys, ok := analyze.FilterPoints(buf, int(c.levels), c.eta)
+	if !ok { return nil, false }
+	cs, _ := analyze.PennaVolumeFit(pxs, pys, halo, int(c.order), int(c.order))
+	return cs, true
+}
+
+func binBySnap(snaps, ids []int) (snapBins, idxBins map[int][]int) {
+	snapBins = make(map[int][]int)
+	idxBins = make(map[int][]int)
+	for i, snap := range snaps {
+		id := ids[i]
+		snapBins[snap] = append(snapBins[snap], id)
+		idxBins[snap] = append(idxBins[snap], i)
+	}
+	return snapBins, idxBins
 }

be2e45ecac9b6be451f198b7fbf86efc909fb05e
Phil Mansfield <mansfield@uchicago.edu>
2016-12-01 09:38:04 -0600

    Added download script.

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+go get github.com/gonum/matrix
+go get github.com/gonum/floats
+go get github.com/gonum/internal/asm
+go get github.com/phil-mansfield/consistent_trees
+go get github.com/phil-mansfield/go-artio
+go get github.com/phil-mansfield/shellfish
+go install github.com/phil-mansfield/shellfish

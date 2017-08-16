241b0b79ca43b0e66200d4334db5e09b221ab61f
phil-mansfield <mansfield@uchicago.edu>
2017-08-16 12:44:25 -0500

    Fixed units on RMax and RMin.

--- cmd/stats.go
+++ cmd/stats.go
@@ -336,7 +336,7 @@ func (config *StatsConfig) Run(
 			"Intermediate Axis [cMpc/h]",
 			"Minor Axis [cMpc/h]",
 			"Ax", "Ay", "Az",
-			"RMin [Mpc/h]", "RMax [Mpc/h]",
+			"RMin [cMpc/h]", "RMax [cMpc/h]",
 		},
 		[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
 		[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
